40fd7e89ba64d6e316fea4887dea67c0c8240e1a
JoaoPSFranca <francasjoaopedro@gmail.com>
2025-05-11 22:50:04 -0300

    fix: traduzindo o script do banco

--- Banco/Script.sql
+++ Banco/Script.sql
@@ -2,115 +2,112 @@ DROP DATABASE IF EXISTS cara_cara;
 CREATE DATABASE cara_cara;
 USE cara_cara;
 
-CREATE TABLE game_session (
-	session_id INT NOT NULL AUTO_INCREMENT,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME,
-    difficulty_level INT DEFAULT 1,
-    PRIMARY KEY (session_id)
+CREATE TABLE funcao_celula (
+	funcao_id INT NOT NULL AUTO_INCREMENT,
+	funcao_nome VARCHAR(100) NOT NULL,
+    funcao_desc TEXT NOT NULL,
+    PRIMARY KEY (funcao_id)
 );
 
-CREATE TABLE cell_function (
-	function_id INT NOT NULL AUTO_INCREMENT,
-    function_name VARCHAR(100) NOT NULL,
-    function_desc TEXT NOT NULL,
-    PRIMARY KEY (function_id)
+CREATE TABLE jogador (
+	jogador_id INT NOT NULL AUTO_INCREMENT,
+    jogador_nome VARCHAR(100) NOT NULL,
+    vencedor TINYINT(1) DEFAULT '0',
+    PRIMARY KEY (jogador_id)
 );
 
-CREATE TABLE kingdom (
-	king_id INT NOT NULL AUTO_INCREMENT,
-    king_name VARCHAR(50) NOT NULL,
-    king_desc TEXT NOT NULL,
-    PRIMARY KEY (king_id)
+CREATE TABLE organela (
+	organela_id INT NOT NULL AUTO_INCREMENT,
+    organela_nome VARCHAR(100) NOT NULL,
+    organela_desc TEXT NOT NULL,
+    PRIMARY KEY (organela_id)
 );
 
-CREATE TABLE property (
+CREATE TABLE propriedade (
 	prop_id INT NOT NULL AUTO_INCREMENT,
-    prop_name TEXT NOT NULL,
+    prop_nome TEXT NOT NULL,
     PRIMARY KEY (prop_id)
 );
 
-CREATE TABLE organelle (
-	org_id INT NOT NULL AUTO_INCREMENT,
-    org_name VARCHAR(100) NOT NULL,
-    org_desc TEXT NOT NULL,
-    PRIMARY KEY (org_id)
+CREATE TABLE reino (
+	reino_id INT NOT NULL AUTO_INCREMENT,
+    reino_nome VARCHAR(50) NOT NULL,
+    reino_desc TEXT NOT NULL,
+    PRIMARY KEY (reino_id)
 );
 
-CREATE TABLE card (
-	card_id INT NOT NULL AUTO_INCREMENT,
-    card_name VARCHAR(100) NOT NULL,
-    card_desc TEXT NOT NULL,
-    card_is_cell BOOLEAN NOT NULL,
-    card_image BLOB,
-    card_difficulty_level INT DEFAULT 1,
-    card_active BOOLEAN NOT NULL DEFAULT TRUE,
-	king_id INT NOT NULL,
-    PRIMARY KEY (card_id),
-    FOREIGN KEY (king_id) REFERENCES kingdom (king_id)
+CREATE TABLE sessao_jogo (
+	sessao_id INT NOT NULL AUTO_INCREMENT,
+    sessao_inicio DATETIME NOT NULL,
+    sessao_fim DATETIME,
+    nivel_dificuldade INT DEFAULT 1,
+    PRIMARY KEY (sessao_id)
 );
 
-CREATE TABLE team (
-	team_id INT NOT NULL AUTO_INCREMENT,
-    team_name VARCHAR(100) NOT NULL,
-    is_winner BOOLEAN DEFAULT FALSE,
-	session_id INT NOT NULL, 
-    assigned_card_id INT, 
-    guessed_card_id INT,
-    PRIMARY KEY (team_id),
-    FOREIGN KEY (session_id) REFERENCES game_session (session_id),
-    FOREIGN KEY (assigned_card_id) REFERENCES card (card_id),
-    FOREIGN KEY (guessed_card_id) REFERENCES card (card_id) 
+CREATE TABLE carta (
+	carta_id INT NOT NULL AUTO_INCREMENT,
+    carta_nome VARCHAR(100) NOT NULL,
+    carta_desc TEXT NOT NULL,
+    carta_celula TINYINT(1) NOT NULL,
+    carta_nivel_dificuldade INT DEFAULT 1,
+    reino_id INT NOT NULL,
+    PRIMARY KEY (carta_id),
+    FOREIGN KEY (reino_id) REFERENCES reino (reino_id)
 );
 
-CREATE TABLE game_question (
-	question_id INT NOT NULL AUTO_INCREMENT,
-    session_id INT NOT NULL,
-    team_id INT NOT NULL,
-    question_text TEXT NOT NULL,
-    question_answer VARCHAR(3),
-    question_time DATETIME NOT NULL,
-    PRIMARY KEY (question_id, session_id),
-    FOREIGN KEY (session_id) REFERENCES game_session (session_id),
-    FOREIGN KEY (team_id) REFERENCES team (team_id)
+CREATE TABLE carta_organela (
+	organela_id INT NOT NULL,
+    carta_id INT NOT NULL,
+    PRIMARY KEY (organela_id, carta_id),
+    FOREIGN KEY (organela_id) REFERENCES organela (organela_id),
+    FOREIGN KEY (carta_id) REFERENCES carta (carta_id)
 );
 
-CREATE TABLE card_property (
+CREATE TABLE funcao_carta (
+	funcao_id INT NOT NULL,
+    carta_id INT NOT NULL,
+    PRIMARY KEY (funcao_id, carta_id),
+    FOREIGN KEY (funcao_id) REFERENCES funcao_celula (funcao_id),
+    FOREIGN KEY (carta_id) REFERENCES carta (carta_id)
+);
+
+CREATE TABLE propriedade_carta (
 	prop_id INT NOT NULL,
-    card_id INT NOT NULL,
-    card_prop_desc TEXT NOT NULL,
-	PRIMARY KEY (prop_id, card_id),
-    FOREIGN KEY (prop_id) REFERENCES property (prop_id),
-    FOREIGN KEY (card_id) REFERENCES card (card_id)
+    carta_id INT NOT NULL,
+    carta_prop_desc TEXT NOT NULL,
+    PRIMARY KEY (prop_id, carta_id),
+    FOREIGN KEY (prop_id) REFERENCES propriedade (prop_id),
+    FOREIGN KEY (carta_id) REFERENCES carta (carta_id)
 );
 
-CREATE TABLE card_organelle (
-	org_id INT NOT NULL,
-    card_id INT NOT NULL,
-    PRIMARY KEY (org_id, card_id),
-    FOREIGN KEY (org_id) REFERENCES organelle (org_id),
-    FOREIGN KEY (card_id) REFERENCES card (card_id)
+CREATE TABLE times (
+	sessao_id INT NOT NULL,
+    jogador_id INT NOT NULL,
+    PRIMARY KEY (sessao_id, jogador_id),
+    FOREIGN KEY (sessao_id) REFERENCES sessao_jogo (sessao_id),
+    FOREIGN KEY (jogador_id) REFERENCES jogador (jogador_id)
 );
 
-CREATE TABLE card_function (
-	function_id INT NOT NULL,
-    card_id INT NOT NULL,
-    PRIMARY KEY (function_id, card_id),
-    FOREIGN KEY (function_id) REFERENCES cell_function (function_id),
-    FOREIGN KEY (card_id) REFERENCES card (card_id)
+CREATE TABLE questao (
+	questao_id INT NOT NULL,
+    sessao_id INT NOT NULL,
+    jogador_id INT NOT NULL,
+    questao_texto TEXT NOT NULL,
+    questao_resposta VARCHAR(3) NOT NULL,
+    PRIMARY KEY (questao_id, sessao_id, jogador_id),
+    FOREIGN KEY (sessao_id, jogador_id) REFERENCES times (sessao_id, jogador_id)
 );
 
-CREATE TABLE game_card_status (
-    status_id INT NOT NULL AUTO_INCREMENT,
-    session_id INT NOT NULL,
-    card_id INT NOT NULL,
-    team_id INT NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    deactivation_time DATETIME,
-    deactivation_question_id INT,
-    PRIMARY KEY (status_id),
-    FOREIGN KEY (session_id) REFERENCES game_session (session_id),
-    FOREIGN KEY (card_id) REFERENCES card (card_id),
-    FOREIGN KEY (deactivation_question_id, session_id) REFERENCES game_question (question_id, session_id),
-    FOREIGN KEY (team_id) REFERENCES team (team_id)
+CREATE TABLE jogo_carta_status (
+	jogador_id INT NOT NULL,
+    sessao_id INT NOT NULL,
+    carta_id INT NOT NULL,
+    ativa TINYINT(1) DEFAULT '1',
+    sorteada TINYINT(1) DEFAULT '0',
+    advinhada TINYINT(1) DEFAULT '0',
+    questao_id INT NOT NULL,
+    PRIMARY KEY (jogador_id, sessao_id, carta_id),
+    FOREIGN KEY (jogador_id, sessao_id) REFERENCES times (jogador_id, sessao_id),
+    FOREIGN KEY (carta_id) REFERENCES carta (carta_id),
+    FOREIGN KEY (questao_id) REFERENCES questao (questao_id)
 );
