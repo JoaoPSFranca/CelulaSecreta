ba55c8a29858914155272d82bdfbdca547e779da
JoaoPSFranca <francasjoaopedro@gmail.com>
2026-03-09 15:00:23 -0300

    fix: fixing questions and making better questions and answers

--- src/CelulaSecretaV2/src/main/java/br/edu/ifsp/pep/service/CartaService.java
+++ src/CelulaSecretaV2/src/main/java/br/edu/ifsp/pep/service/CartaService.java
@@ -6,6 +6,8 @@ import br.edu.ifsp.pep.model.Reino;
 import java.util.List;
 import java.util.ArrayList;
 
+//adipocito e candida albicans
+
 public class CartaService {
     public List<Carta> carregarCartas() {
         List<Carta> cartas = new ArrayList<>();
@@ -20,106 +22,106 @@ public class CartaService {
         cartas.add(new Carta(1, "Adipócito", "Adipocito", "Célula que armazena gordura.", true, 1, animalia,
                 List.of("Eucarionte", "Heterótrofo"),
                 List.of("Armazenamento de energia", "Proteção térmica"),
-                List.of("mitocôndria", "membrana plasmática", "núcleo", "complexo golgiense", "gotículas lipídicas")));
+                List.of("citoplasma", "complexo_golgiense", "membrana_plasmatica", "mitocondria", "nucleo", "reserva_de_gordura")));
 
         cartas.add(new Carta(2, "Célula Animal", "Celula_Animal", "Modelo de célula eucarionte animal.", true, 1, animalia,
                 List.of("Eucarionte", "Heterótrofo"),
                 List.of("Metabolismo", "Divisão celular"),
-                List.of("citoesqueleto", "mitocôndria", "lisossomo", "retículo endoplasmático", "membrana celular", "ribossomo", "centríolo", "complexo golgiense", "vacúolo", "nucleolo", "membrana nuclear")));
+                List.of("centriolo", "citoesqueleto", "complexo_golgiense", "lisossomo", "membrana_nuclear", "membrana_plasmatica", "mitocondria", "nucleo", "reticulo_endoplasmatico", "ribossomo", "vacuolo")));
 
         cartas.add(new Carta(3, "Célula B Pancreática", "Celula_b_pancreatica", "Célula do pâncreas que produz insulina.", true, 3, animalia,
                 List.of("Eucarionte", "Secretora"),
                 List.of("Recebe: insulina, glucose", "Produz: ca++, atp, k+", "Ejeta: vdcc"),
-                List.of("glut2", "mitocôndria", "núcleo", "retículo endoplasmático", "complexo golgiense", "vesículas de insulina")));
+                List.of("atp", "ca++", "glucose", "glut2", "insulina", "k+", "mitocondria", "vdcc")));
 
         cartas.add(new Carta(4, "Célula Caliciforme", "Celula_calciforme", "Célula secretora de muco encontrada em epitélios.", true, 2, animalia,
                 List.of("Eucarionte", "Secretora"),
                 List.of("Produção de muco", "Lubrificação"),
-                List.of("microviscosidade", "vesículas secretoras", "retículo endoplasmático", "complexo golgiense", "núcleo")));
+                List.of("complexo_golgiense", "microvilosidades", "nucleo", "reticulo_endoplasmatico", "vesicula_secretora")));
 
         cartas.add(new Carta(5, "Célula de Leydig", "Celula_Leydig", "Célula produtora de testosterona nos testículos.", true, 3, animalia,
                 List.of("Eucarionte", "Secretora"),
                 List.of("Produção de hormônios"),
-                List.of("retículo endoplasmático", "mitocôndria", "gotículas lipídicas", "núcleo", "nucléolo")));
+                List.of("goticulas_lipidicas", "mitocondria", "nucleo", "nucleolo", "reticulo_endoplasmatico")));
 
         cartas.add(new Carta(6, "Enterócito", "Enterocito", "Célula do epitélio intestinal.", true, 2, animalia,
                 List.of("Eucarionte", "Absortiva"),
                 List.of("Absorção de nutrientes"),
-                List.of("vacúolo", "citoplasma", "membrana celular", "mitocôndria", "complexo golgiense", "dna", "membrana nuclear", "microvilosidades")));
+                List.of("citoplasma", "complexo_golgiense", "dna", "membrana_celular", "membrana_nuclear", "microvilosidades", "mitocondria", "vacuolo")));
 
         cartas.add(new Carta(7, "Macrófago", "Macrofago", "Célula do sistema imune que realiza fagocitose.", true, 2, animalia,
                 List.of("Eucarionte", "Fagocitária"),
                 List.of("Defesa do organismo", "Fagocitose"),
-                List.of("lisossomo", "complexo golgiense", "mitocôndria", "retículo endoplasmático", "ribossomo", "pseudópodes", "núcleo")));
+                List.of("complexo_golgiense", "lisossomo", "mitocondria", "pseudopode", "reticulo_endoplasmatico", "ribossomo")));
 
         cartas.add(new Carta(8, "Neurônio", "Neuronio", "Célula do sistema nervoso.", true, 2, animalia,
                 List.of("Eucarionte", "Excitável"),
                 List.of("Transmissão de impulsos nervosos"),
-                List.of("ribossomo", "citoplasma", "membrana celular", "mitocôndria", "membrana nuclear", "dna", "dendritos", "axônio", "núcleo")));
+                List.of("axonio", "citoplasma", "dendrito", "dna", "membrana_celular", "membrana_nuclear", "mitocondria", "ribossomo")));
 
         cartas.add(new Carta(9, "Neutrófilo", "Neutrofilo", "Tipo de leucócito, linha de frente da defesa imune.", true, 2, animalia,
                 List.of("Eucarionte", "Fagocitária"),
                 List.of("Defesa do organismo", "Fagocitose"),
-                List.of("grânulo primário", "grânulo secundário", "mitocôndria", "núcleo")));
+                List.of("granulos", "mitocondria", "nucleo")));
 
         // --- Cartas do Reino Plantae ---
         cartas.add(new Carta(10, "Célula Vegetal", "Celula_Vegetal", "Modelo de célula eucarionte vegetal.", true, 1, plantae,
                 List.of("Eucarionte", "Autótrofo", "Parede Celular de Celulose"),
                 List.of("Fotossíntese", "Respiração Celular"),
-                List.of("complexo golgiense", "lisossomo", "vacúolo", "mitocôndria", "cloroplasto", "ribossomo", "retículo endoplasmático rugoso", "retículo endoplasmático liso", "núcleo", "nucléolo", "parede celular de celulose")));
+                List.of("cloroplasto", "complexo_golgiense", "lisossomo", "mitocondria", "nucleo", "nucleolo", "reticulo_endoplasmatica", "ribossomo", "vacuolo")));
 
         cartas.add(new Carta(11, "Célula-Guarda", "Celula_Guarda", "Controla a abertura e fechamento dos estômatos.", true, 2, plantae,
                 List.of("Eucarionte", "Autótrofo"),
                 List.of("Trocas gasosas", "Controle hídrico"),
-                List.of("núcleo", "ostíolo", "epiderme", "cloroplasto", "parede celular de celulose")));
+                List.of("cloroplasto", "epiderme", "nucleo", "ostiolo")));
 
         // --- Cartas do Reino Fungi ---
         cartas.add(new Carta(12, "Candida albicans", "Candida_albicans", "Fungo do tipo levedura, pode ser patogênico.", false, 2, fungi,
                 List.of("Eucarionte", "Unicelular", "Parede Celular de Quitina"),
                 List.of("Fermentação", "Decomposição"),
-                List.of("mitocôndria", "membrana nuclear", "dna", "vacúolo", "ribossomo", "complexo golgiense", "parede celular de quitina", "membrana celular", "brotos", "núcleo")));
+                List.of("broto", "complexo_golgiense", "dna", "membrana_celular", "membrana_nuclear", "mitocondria", "ribossomo", "vacuolo")));
 
         cartas.add(new Carta(13, "Rhizopus sp", "Rhizopus_sp", "Gênero de bolores comuns em pães e frutas.", false, 1, fungi,
                 List.of("Eucarionte", "Multicelular", "Parede Celular de Quitina"),
                 List.of("Decomposição"),
-                List.of("citoplasma", "vacúolo", "dna", "membrana nuclear", "parede celular de quitina", "membrana celular", "mitocôndria", "retículo", "esporos", "núcleo")));
+                List.of("citoplasma", "dna", "esporos", "membrana_celular", "membrana_nuclear", "mitocondria", "parede_celular", "reticulo_endoplasmatico", "vacuolo")));
 
         // --- Cartas do Reino Protista ---
         cartas.add(new Carta(14, "Chlamydomonas sp", "Chlamydomonas_sp", "Gênero de algas verdes unicelulares.", false, 2, protista,
                 List.of("Eucarionte", "Unicelular", "Autótrofo", "Móvel"),
                 List.of("Fotossíntese", "Locomoção"),
-                List.of("flagelos", "mitocôndria", "núcleo", "grão de amido", "pirenoide", "complexo golgiense", "cloroplasto", "vacúolo")));
+                List.of("cloroplasto", "complexo_golgiense", "flagelo", "grao_de_amido", "mitocondria", "pirenoide", "vacuolo")));
 
         cartas.add(new Carta(15, "Paramecium sp", "Paramecio", "Protozoário ciliado de água doce.", false, 2, protista,
                 List.of("Eucarionte", "Unicelular", "Móvel"),
                 List.of("Locomoção", "Fagocitose"),
-                List.of("vacuola contráctil", "canal radial", "micronúcleo", "citoplasma", "vacuolas de alimentos", "poro anal", "boca celular", "apertura bucal", "surco oral", "membrana plasmática", "cílios")));
+                List.of("abertura_bucal", "boca_celular", "canal_radial", "cilios", "citoplasme", "macronucleo", "membrana_plasmatica", "micronucleo", "poro_anal", "vacuola_contractil", "vacuola_de_alimento")));
 
         cartas.add(new Carta(16, "Plasmodium vivax", "Plasmodium_vivax", "Protozoário causador de uma forma de malária.", false, 3, protista,
                 List.of("Eucarionte", "Unicelular", "Parasita"),
                 List.of("Ciclo de vida complexo"),
-                List.of("roptria", "citoplasma", "mitocôndria", "apicoplasto", "retículo endoplasmático", "núcleo", "complexo golgiense", "grânulos densos")));
+                List.of("apicoplasto", "citoplasma", "complexo_golgiense", "granulo_denso", "mitocondria", "nucleo", "reticulo_endoplasmatico", "roptria")));
 
         cartas.add(new Carta(17, "Trypanosoma cruzi", "Trypanosoma_cruzi", "Protozoário flagelado causador da Doença de Chagas.", false, 3, protista,
                 List.of("Eucarionte", "Unicelular", "Móvel", "Parasita"),
                 List.of("Locomoção"),
-                List.of("mitocôndria", "ribossomo", "retículo", "membrana ondulatória", "membrana nuclear", "dna", "complexo golgiense", "vacúolo", "flagelo", "núcleo")));
+                List.of("complexo_golgiense", "dna", "membrana_necluar", "membrana_ondulatoria", "mitocondria", "reticulo_endoplasmatico", "ribossomo", "vacuolo")));
 
         // --- Cartas do Reino Monera ---
         cartas.add(new Carta(18, "Escherichia coli", "Escherichia_coli", "Bactéria comum no intestino de mamíferos.", false, 1, monera,
                 List.of("Procarionte", "Unicelular"),
                 List.of("Fermentação", "Respiração anaeróbica"),
-                List.of("membrana celular", "parede celular de peptideoglicano", "citoplasma", "ribossomo", "dna", "plasmídio", "fímbria")));
+                List.of("citoplasma", "dna", "fimbria", "membrana_celular", "parede_celular", "plasmidio", "ribossomo")));
 
         cartas.add(new Carta(19, "Streptococcus sp", "Streptococcus_sp", "Gênero de bactérias em forma de coco, em cadeias.", false, 1, monera,
                 List.of("Procarionte", "Unicelular"),
                 List.of("Fermentação"),
-                List.of("ribossomo", "citoplasma", "dna", "parede celular de peptideoglicano", "membrana plasmática", "glicocálice")));
+                List.of("citoplasma", "dna", "glicocalice", "membrana_plasmatica", "parede_celular", "ribossomo")));
 
         cartas.add(new Carta(20, "Treponema pallidum", "Treponema_pallidum", "Bactéria em forma de espiral, causadora da sífilis.", false, 3, monera,
                 List.of("Procarionte", "Unicelular", "Móvel"),
                 List.of("Locomoção"),
-                List.of("plasmídio", "citoplasma", "dna", "ribossomo", "membrana celular", "parede celular de peptideoglicano", "flagelo axial")));
+                List.of("citoplasma", "dna", "flagelo_axial", "membrana_celular", "parede_celular", "plasmidio", "ribossomo")));
 
         return cartas;
     }
--- src/CelulaSecretaV2/src/main/java/br/edu/ifsp/pep/service/PerguntaService.java
+++ src/CelulaSecretaV2/src/main/java/br/edu/ifsp/pep/service/PerguntaService.java
@@ -22,136 +22,111 @@ public class PerguntaService {
         perguntas.add(new Pergunta(id++, "Pertence ao Reino Plantae?",
                 carta -> carta.getReino().getNome().equalsIgnoreCase("Plantae")));
 
-        // --- Perguntas sobre Organelas e Estruturas Comuns ---
-        perguntas.add(new Pergunta(id++, "Possui Núcleo definido?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("núcleo") || o.equalsIgnoreCase("membrana nuclear"))));
-        perguntas.add(new Pergunta(id++, "Possui Mitocôndria (para respiração celular)?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("mitocôndria"))));
+        // --- Perguntas sobre Organelas e Estruturas ---
+        perguntas.add(new Pergunta(id++, "Possui Núcleo definido? (Núcleo e/ou Membrana Nuclear)",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("nucleo") || o.contains("membrana_nuclear"))));
+        perguntas.add(new Pergunta(id++, "Possui Mitocôndria?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("mitocondria"))));
         perguntas.add(new Pergunta(id++, "Possui Ribossomos?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("ribossomo"))));
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("ribossomo"))));
         perguntas.add(new Pergunta(id++, "Possui Complexo Golgiense?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("complexo golgiense"))));
-        perguntas.add(new Pergunta(id++, "Possui Membrana Plasmática/Celular?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("membrana"))));
-
-        // --- Perguntas sobre Parede Celular (MODIFICADAS E NOVAS) ---
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("complexo_golgiense"))));
+        perguntas.add(new Pergunta(id++, "Possui Membrana Plasmática?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("membrana_plasmatica"))));
+        perguntas.add(new Pergunta(id++, "Possui Membrana Celular?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("membrana_celular"))));
         perguntas.add(new Pergunta(id++, "Possui Parede Celular?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("parede celular"))));
-
-        perguntas.add(new Pergunta(id++, "Possui parede celular de quitina?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("parede celular de quitina"))));
-
-        perguntas.add(new Pergunta(id++, "Possui parede celular de celulose?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("parede celular de celulose"))));
-
-        perguntas.add(new Pergunta(id++, "Possui parede celular de peptideoglicano?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("parede celular de peptideoglicano"))));
-
-        // --- Perguntas sobre Estruturas Específicas e Diferenciadoras ---
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("parede_celular"))));
+        perguntas.add(new Pergunta(id++, "Possui DNA aparente? (Está representado na imagem?)",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("dna"))));
+        perguntas.add(new Pergunta(id++, "Possui Retículo representado?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("reticulo"))));
+        perguntas.add(new Pergunta(id++, "Possui Vacúolo?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("vacuolo"))));
         perguntas.add(new Pergunta(id++, "Possui Cloroplasto (para fotossíntese)?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("cloroplasto"))));
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("cloroplasto"))));
         perguntas.add(new Pergunta(id++, "Possui Lisossomos (para digestão celular)?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("lisossomo"))));
-        perguntas.add(new Pergunta(id++, "Possui Flagelos ou Cílios para locomoção?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("flagelos") || o.equalsIgnoreCase("flagelo") || o.equalsIgnoreCase("cílios"))));
-        perguntas.add(new Pergunta(id++, "É um organismo parasita?",
-                carta -> carta.getPropriedades().stream().anyMatch(p -> p.equalsIgnoreCase("parasita"))));
-        perguntas.add(new Pergunta(id++, "Realiza fagocitose (é uma célula 'fagocitária')?",
-                carta -> carta.getPropriedades().stream().anyMatch(p -> p.equalsIgnoreCase("fagocitária"))));
-        perguntas.add(new Pergunta(id++, "Seu material genético (DNA) fica em um Nucleoide?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("nucleoide"))));
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("lisossomo"))));
+        perguntas.add(new Pergunta(id++, "Possui nucleolo?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("nucleolo"))));
+        perguntas.add(new Pergunta(id++, "Possui Microvilosidades?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("microvilosidades"))));
+        perguntas.add(new Pergunta(id++, "Possui Plasmídio?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("plasmidio"))));
 
-        // --- NOVAS PERGUNTAS ESPECÍFICAS POR CÉLULA ---
-
-        // Célula β pancreática
-        perguntas.add(new Pergunta(id++, "Apresenta transportador de glicose (GLUT2)?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("glut2"))));
-
-        perguntas.add(new Pergunta(id++, "Realiza secreção de insulina?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("insulina"))));
+        // --- Perguntas específicas ---
 
         // Adipócito
-        perguntas.add(new Pergunta(id++, "Armazena gordura?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("gotículas lipídicas")) ||
-                        carta.getFuncoes().stream().anyMatch(f -> f.toLowerCase().contains("armazenamento de energia"))));
-
-        // Célula Guarda
-        perguntas.add(new Pergunta(id++, "Apresenta ostíolo?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("ostíolo"))));
-
-        // Neurônio
-        perguntas.add(new Pergunta(id++, "Possui dendritos?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("dendritos"))));
-
-        perguntas.add(new Pergunta(id++, "Possui axônio?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("axônio"))));
-
-        // Célula de Leydig
-        perguntas.add(new Pergunta(id++, "Apresenta pequenas gotículas lipídicas?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("gotículas lipídicas"))));
-
-        // Rhizopus sp
-        perguntas.add(new Pergunta(id++, "Apresenta esporos?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("esporos"))));
-
-        // Célula Caliciforme
-        perguntas.add(new Pergunta(id++, "Possui vesícula secretora?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("vesículas secretoras"))));
-
-        // Enterócito
-        perguntas.add(new Pergunta(id++, "Possui Microvilosidades?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("microvilosidades"))));
-
-        // Neutrófilo
-        perguntas.add(new Pergunta(id++, "Apresenta grânulo primário?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("grânulo primário"))));
-
-        // Célula Vegetal
-        perguntas.add(new Pergunta(id++, "Apresenta vacúolo bem desenvolvido?",
-                carta -> carta.getNome().equalsIgnoreCase("Célula Vegetal") &&
-                        carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("vacúolo"))));
-
-        // Paramecium sp
-        perguntas.add(new Pergunta(id++, "Presença de vacúolo contrátil?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("vacuola contráctil"))));
-
-        perguntas.add(new Pergunta(id++, "Presença de cílios?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("cílios"))));
-
-        // Plasmodium vivax
-        perguntas.add(new Pergunta(id++, "Apresenta apicoplasto?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("apicoplasto"))));
-
-        // Chlamydomonas sp
-        perguntas.add(new Pergunta(id++, "Presença de pirenoide?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("pirenoide"))));
-
-        perguntas.add(new Pergunta(id++, "Presença de grão de amido?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("grão de amido"))));
-
-        // Trypanosoma cruzi
-        perguntas.add(new Pergunta(id++, "Apresenta membrana ondulante?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.toLowerCase().contains("membrana ondulatória"))));
-
-        // Escherichia coli
-        perguntas.add(new Pergunta(id++, "Presença de fímbria?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("fímbria"))));
-
-        // Streptococcus sp
-        perguntas.add(new Pergunta(id++, "Apresenta glicocálice?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("glicocálice"))));
-
-        // Treponema pallidum
-        perguntas.add(new Pergunta(id++, "Apresenta flagelo axial?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("flagelo axial"))));
-
-        // Macrófago
-        perguntas.add(new Pergunta(id++, "Apresenta pseudópodes?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("pseudópodes"))));
+        perguntas.add(new Pergunta(id++, "A Célula possui Reserva de Gordura?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("reserva_de_gordura"))));
 
         // Candida albicans
-        perguntas.add(new Pergunta(id++, "Apresenta brotos?",
-                carta -> carta.getOrganelas().stream().anyMatch(o -> o.equalsIgnoreCase("brotos"))));
+        perguntas.add(new Pergunta(id++, "A Célula possui Broto?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("broto"))));
+
+        // Célula Animal
+        perguntas.add(new Pergunta(id++, "A Célula possui Centríolo e Citoesqueleto representados?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("centriolo") && o.contains("citoesqueleto"))));
+
+        // Célula b Pancreática
+        perguntas.add(new Pergunta(id++, "A Célula produz Insulina?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("insulina"))));
+
+        // Célula Calciforme
+        perguntas.add(new Pergunta(id++, "A Célula possui Vesícula Secretora?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("vesicula_secretora"))));
+
+        // Celula de Leydig
+        perguntas.add(new Pergunta(id++, "A Célula possui Gotículas Lipídicas?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("goticulas_lipidicas"))));
+
+        // Célula Guarda
+        perguntas.add(new Pergunta(id++, "A Célula possui Ostíolo e Epiderme?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("ostiolo") && o.contains("epiderme"))));
+
+        // Chlamydomonas sp
+        perguntas.add(new Pergunta(id++, "A Célula possui Pirenoide e Grãos de Amido?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("pirenoide") && o.contains("graos_de_amido"))));
+
+        // Escherichia coli
+        perguntas.add(new Pergunta(id++, "A Célula possui Fímbria?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("fimbria"))));
+
+        // Macrófago
+        perguntas.add(new Pergunta(id++, "A Célula possui Pseudópode?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("pseudopode"))));
+
+        // Neurônio
+        perguntas.add(new Pergunta(id++, "A Célula possui Axônio e Dendritos?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("axonio") && o.contains("dendritos"))));
+
+        // Neutrofilo
+        perguntas.add(new Pergunta(id++, "A Célula possui um núcleo em formato de ferradura?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("nucleo_ferradura"))));
+
+        // Paramecio
+        perguntas.add(new Pergunta(id++, "A Célula possui Cílios?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("cilios"))));
+
+        // Plasmodium Vivax
+        perguntas.add(new Pergunta(id++, "A Célula possui Roptria?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("roptria"))));
+
+        // Rhizopus sp
+        perguntas.add(new Pergunta(id++, "A Célula possui Esporos?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("esporos"))));
+
+        // Streptococcus sp
+        perguntas.add(new Pergunta(id++, "A Célula possui glicocalice?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("glicocalice"))));
+
+        // Treponema paliddum
+        perguntas.add(new Pergunta(id++, "A Célula possui flagelo axial?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("flagelo_axial"))));
+
+        // Trypanosoma cruzi
+        perguntas.add(new Pergunta(id++, "A Célula possui Membrana Ondulatória?",
+                carta -> carta.getOrganelas().stream().anyMatch(o -> o.contains("membrana_ondulatoria"))));
 
         return perguntas;
     }
